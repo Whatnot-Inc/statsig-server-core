01ba08c9f3385f6678fe4ee8007f5acb9d83e88e
Zach Allaun <zach.allaun@whatnot.com>
2025-10-22 13:30:39 -0400

    0.10.2-dev

--- statsig-elixir/mix.exs
+++ statsig-elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Statsigelixir.MixProject do
   def project do
     [
       app: :statsig_elixir,
-      version: "0.10.2",
+      version: "0.10.2-dev",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
